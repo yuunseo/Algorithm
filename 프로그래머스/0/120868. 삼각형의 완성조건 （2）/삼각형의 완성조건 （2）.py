1836be52032eb9bd814444aff264d52325ffd6d3
yuunseo <96402411+yuunseo@users.noreply.github.com>
2025-04-14 19:23:40 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -6,35 +6,20 @@
     2) x가 가장 긴 변이다
     - 나머지 값들의 합 > x >나머지 값 중 최대값
 - 개수를 구하기 위해서는 최대를 넘지 않을 때까지 +1씩 count를 올린다
+
+-> 두 범위를 합쳐서 하나로 구현하자!
+    - max-min+1 <= x <= max+min-1
 2. 시간복잡도
 - 더한 값 ~ 최대값 개수 n: O(N)
 3. 변수
 - int: 정수의 개수
 '''
-def count_valid_len(minitem,maxitem):
-    start = maxitem- minitem+ 1
-    end = maxitem
-    if start > end:
-        return 0
-    return end - start + 1
-
-def count_valid_len_out(minitem,maxitem):
-    start = maxitem+1
-    end = minitem+maxitem-1
-    if start > end:
-        return 0
-    return end - start + 1
 
 def solution(sides):
 
     maxitem = max(sides)
     minitem = min(sides)
+
+    result = sum(sides) - (maxitem - minitem) - 1
     
-    # 1) 가장 긴 변이 sides 안에 있는 경우
-    #print(count_valid_len(minitem,maxitem))
-    
-    # 2) 가장 긴 변이 sides 밖에 있는 경우
-    #print(count_valid_len_out(minitem,maxitem))
-    
-    return count_valid_len(minitem,maxitem) + count_valid_len_out(minitem,maxitem)
-    
+    return result
