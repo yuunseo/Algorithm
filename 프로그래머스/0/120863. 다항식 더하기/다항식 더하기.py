df8c020832c2acab82da36f9912fd195828ef385
yuunseo <96402411+yuunseo@users.noreply.github.com>
2025-04-15 21:59:48 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,34 +1,19 @@
-'''
-1. 아이디어
-    - 공백을 구분자로 나누어 저장한다
-    - x를 포함하는 항일 경우, x를 제외한 앞 정수들끼리 더한다
-    - x를 포함 안하는 항일 경우, 정수들끼리 더한다
-'''
 def solution(polynomial):
-    List = polynomial.split(" ")
-    
+    terms = polynomial.split()
+
     xnum = 0
     num = 0
 
-    for i in List:
-        if 'x' in i:
-            if len(i)>1:
-                xnum += int(i[:-1])
-            else:
-                xnum += 1
-        elif 'x' not in i and i.isdigit(): # '+'는 처리하지 않는다
-            num += int(i)
-    
+    for term in terms:
+        if 'x' in term:
+            xnum += int(term[:-1]) if term != 'x' else 1
+        elif term.isdigit():
+            num += int(term)
 
-    result = ""
-    if xnum > 1:
-        result += str(xnum)+"x"
-    elif xnum == 1:
-        result += "x"
-    
-    if num != 0 and result != "":
-        result += " + "+str(num)
-    elif num != 0 and result == "":
-        result += str(num)
-    
-    return result
+    result = []
+    if xnum:
+        result.append(f"{xnum}x" if xnum > 1 else "x")
+    if num:
+        result.append(str(num))
+
+    return " + ".join(result)
