d72bda8c8987bfcfad4152cb71e68da6ffabbb53
yuunseo <96402411+yuunseo@users.noreply.github.com>
2025-04-17 12:04:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -5,37 +5,28 @@
     2) 고유번호 선택하기
         - 고유번호 i번의 장르의 노래가 plays[i]번 재생되었을 경우, 많이 재생된 장르 먼저 수록한다
     3) 장르별 최대 2개까지 선택 가능 & 재생횟수 순 & 고유번호 오름차 순
+    
+    -> 겹치고 반복하는 자료구조를 해시로 활용할 수 있음 : "genre" : (plays, i) > value[0]으로 정렬해서 i를 추가하기.
 '''
 from collections import defaultdict
 
 def solution(genres, plays):
-    
-    # 0. 기본 변수 설정
-    genre = defaultdict(int)
+    # 1. 해시 자료구조 만들기
+    genre_total = defaultdict(int)               # 장르별 총 재생 횟수
+    genre_songs = defaultdict(list)              # 장르별 노래 리스트: (재생수, 고유번호)
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        genre_total[g] += p
+        genre_songs[g].append((p, i))
+
+    # 2. 장르 정렬 (총 재생 수 기준으로 내림차순)
+    sorted_genres = sorted(genre_total.items(), key=lambda x: x[1], reverse=True)
+
     answer = []
-    
-    # 1. genre 순서 구하기: 전체 재생 횟수가 높은 장르부터 정렬
-    for i in range(len(genres)):
-        genre[genres[i]] += plays[i]
-    
-    genreSorted =[ k for k,v in sorted(genre.items(), key=lambda x:x[1],reverse=True)]
-    #print(genreSorted)
-    
-    # 2. genre 순서대로 재생횟수 확인하기
-    for name in genreSorted:
-        
-        # 3. 각 장르별 노래의 재생 횟수 정렬
-        name_score = []
-        for i in range(len(genres)):
-            if name == genres[i]:
-                name_score.append([i,plays[i]])
-                
-        # 4. 각 장르별 노래는 최대 2개까지 수록 가능
-        if len(name_score) >= 2:
-            name_score = sorted(name_score, key=lambda x:x[1],reverse=True)
-            #print(name_score)
-            answer.extend([i[0] for i in name_score[:2]])
-        else:
-            answer.append(name_score[0][0])
+    for genre, _ in sorted_genres:
+        # 3. 해당 장르의 노래들 정렬 (재생 수 내림차순, 고유번호 오름차순)
+        songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
+        # 4. 최대 2개 고르기
+        answer.extend([i for _, i in songs[:2]])
 
     return answer
