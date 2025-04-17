4980e402edc9a694ba5db7ffd9a387b27f993580
yuunseo <96402411+yuunseo@users.noreply.github.com>
2025-04-17 21:51:08 +0900

    [level 2] Title: 가장 큰 수, Time: 58.43 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -2,4 +2,8 @@ def solution(numbers):
     
     numbers = list(map(str, numbers))  # 문자열로 변환
     numbers.sort(key=lambda x: x*3, reverse=True)  # 핵심! 문자열을 3번 반복해서 정렬 기준으로 사용
+    # 3번 반복한 문자열로 정렬하는 이유는, 비교 가능한 자릿수 1000이하로 만들어주어서 비교하면 된다.
+    # 현재는 3 vs 34라고 했을 때 
+    # 1000자리수에서 비교하기 위하여,
+    # 333 vs 343434 -> 343이 더 큼!
     return '0' if numbers[0] == '0' else ''.join(numbers)
