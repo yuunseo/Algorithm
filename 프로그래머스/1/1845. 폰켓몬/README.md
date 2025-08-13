a0c9667134a0f69d9fb912f01a5b61629e12e965
김윤서 <96402411+yuunseo@users.noreply.github.com>
2025-08-13 22:12:03 +0900

    [level 1] Title: 폰켓몬, Time: 7.94 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 87 MB, 시간: 4.15 ms
+메모리: 85.1 MB, 시간: 7.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 27일 14:27:15
+2025년 08월 13일 22:12:01
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,24 +1,31 @@
 /*
-<아이디어>
-1. 모든 수의 조합 벗, 순서는 상관없이 구하기 -> 이때 서로 다른 종류의 개수만 알면 됨 -> 집합으로 바꾸면 될 듯?
-2. nums.length / 2만큼 조합구하기
->>>> 아니다!
-1. 애초에 우린 서로 다른 종류의 개수들 중, 최대의 수만 알면 되니까 모든 조합의 수를 구할 필요가 없다.
-2. nums의 중복을 없애고, 이게 length/2보다 작다면, 그 size를 반환하고
-3. length/2보다 크다면, length/2를 반환 (그 이상은 안되니까)
-*/
+- 총 N마리 중 N/2마리 가져도 좋아 - 이때 종류의 개수가 많을 수록 좋음 ! 
 
-import java.util.*;
+1. 총 마릿수 N, 가질 수 있는 마릿수 M = N/2
+2. nums를 돌면서 M개씩 조합을 구하기에는 시간 복잡도 매우 큼 !
+3. nums의 폰켓몬 종류를 key로하고 개수를 value로 해서 hashmap에 넣자
+4. 그 담에 map의 길이 >= M이라면, M 반환
+5. map 길이 < M이라면, map 길이 반환
+
+*/
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
     public int solution(int[] nums) {
         
-        Set<Integer> nums_set = new HashSet<Integer>();
-        for(int n: nums){
-            nums_set.add(n);
+        // 1. N과 M 초기화하기
+        int N = nums.length;
+        int M = N/2;
+        
+        // 2. nums를 hashmap으로 변환하기
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for(int i: nums){
+            numsMap.put(i, numsMap.getOrDefault(i,0)+1);
         }
         
-        return Math.min(nums_set.size(),nums.length/2);
+        // 3. map의 길이와 M 비교
+        return (numsMap.size() >= M) ? M : numsMap.size();
         
     }
 }
