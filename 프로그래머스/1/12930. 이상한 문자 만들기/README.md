9ca61f3c95254ca6f918d736a8b36e931e412311
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-07-14 06:28:28 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
 
 ### 성능 요약
 
-메모리: 64 MB, 시간: 0.05 ms
+메모리: 80.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 07일 21:38:46
+2026년 07월 14일 06:28:26
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,28 @@
+/*
+1) split으로 공백 구분 후, 각 단어에서 짝/홀 판단하기
+2) for문으로 s index로 가져와서, 공백일 때 인덱스 초기화하기
+*/
 class Solution {
     public String solution(String s) {
+        
         StringBuilder sb = new StringBuilder();
         
-        int idx = 0;
-        for(char c: s.toCharArray()){
-            if(c==' '){ // 공백 만나면 초기화
-                idx = 0;
-                sb.append(c);
+        int initIndex = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ' '){
+                initIndex = 0;
+                sb.append(' ');
                 continue;
             }
             
-            if(idx%2 == 0){
-                sb.append(Character.toUpperCase(c));
+            if(initIndex % 2 == 0){
+                sb.append(Character.toUpperCase(s.charAt(i)));
             }else{
-                sb.append(Character.toLowerCase(c));
+                sb.append(Character.toLowerCase(s.charAt(i)));
             }
             
-            idx++;
+            initIndex++;
+            
         }
         
         return sb.toString();
