f3b76b04a52b4a1ee407e1551718e35b9f65dda8
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-08-06 15:55:53 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.19 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,55 +1,81 @@
+/*
+Q. N*N 정사각형 경주로 (0으로 이동 가능), 상하좌우 2칸 이상 같은 위치면 직선도로
+A. 최소 비용으로 도착하는 직선도로 개수, 코너 개수 구하기!
+    - 최소 비용 거리 구하기: 다익스트라, 최단 거리더라도 최소 비용을 찾아야 함
+    - 직선 도로 구하기: 다음 땅으로 이동했을 때, 이전 땅의 방향과 현재 땅의 방향 비교하기
+*/
 import java.util.*;
 
 class Solution {
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
+    
+    static int[] dy = new int[] {-1,1,0,0};
+    static int[] dx = new int[] {0,0,-1,1};
+    static int straight = 100;
+    static int corner = 500;
     
     public int solution(int[][] board) {
+        
+        // 변수 초기화
         int n = board.length;
         
-        int[][][] cost = new int[n][n][2];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        // cost[y][x][방향(상하좌우)]
+        // 해당 방향으로 (y, x)에 도착했을 때의 최소 비용
+        // 3번째 인덱스에서는 상하좌우 칸을 만들어두고, 맟는 방향에 기록할 예정
+        int[][][] cost = new int[n][n][4];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 Arrays.fill(cost[i][j], Integer.MAX_VALUE);
             }
         }
         
+        // 경로 구하기
         Queue<int[]> q = new LinkedList<>();
-        
-        // 시작 (방향 없음 → 둘 다 0 처리)
-        q.add(new int[]{0,0,0,0}); 
-        q.add(new int[]{0,0,1,0});
-        
-        cost[0][0][0] = 0;
-        cost[0][0][1] = 0;
+        q.add(new int[] {0,0,-1,0}); // y, x, 이전 방향, 현재까지의 비용
         
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            
             int y = cur[0];
             int x = cur[1];
-            int dir = cur[2];
-            int curCost = cur[3];
+            int prev = cur[2];
+            int totalCost = cur[3];
             
-            for(int d=0; d<4; d++){
+            for(int d=0; d<4; d++){ // 다음으로 이동할 위치 구하기
                 int ny = y + dy[d];
                 int nx = x + dx[d];
                 
-                if(ny<0 || nx<0 || ny>=n || nx>=n) continue;
-                if(board[ny][nx] == 1) continue;
+                if(ny<0 || nx<0 || ny>=n || nx>=n ||
+                  board[ny][nx] == 1) continue;
                 
-                int nextDir = (d < 2) ? 0 : 1;
+                int nextCost; // 다음 위치로 이동
                 
-                int newCost = curCost + 100;
-                if(dir != nextDir) newCost += 500;
-                
-                if(cost[ny][nx][nextDir] > newCost){
-                    cost[ny][nx][nextDir] = newCost;
-                    q.add(new int[]{ny, nx, nextDir, newCost});
+                // 첫 이동이거나 || 같은 방향이면 + 직선 도로
+                if (prev == -1 || prev == d) {
+                    nextCost = totalCost + straight;
+                } else {
+                    // 방향이 바뀌면 + 직선 도로 + 코너
+                    nextCost = totalCost + corner + straight;
+                }
+                 
+                /*
+                 * 같은 위치라도 들어온 방향에 따라
+                 * 이후 비용이 달라질 수 있으므로 방향별로 비교한다.
+                 * nextCost가 더 작다면, 갱신
+                 */
+                if (nextCost < cost[ny][nx][d]) {
+                    cost[ny][nx][d] = nextCost;
+                    q.add(new int[] {ny, nx, d, nextCost});
                 }
             }
         }
         
-        return Math.min(cost[n-1][n-1][0], cost[n-1][n-1][1]);
+        int answer = Integer.MAX_VALUE;
+
+        // 도착점에 각 방향으로 도착한 비용 중 최솟값
+        for (int d = 0; d < 4; d++) {
+            answer = Math.min(answer, cost[n - 1][n - 1][d]);
+        }
+
+        return answer;
+      
     }
 }
