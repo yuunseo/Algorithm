bc3d7bc39401ec9cb7281f5f8b61838fc469afb2
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-08-07 17:59:32 +0900

    [level 3] Title: 여행경로, Time: 0.59 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 107.57 ms
+메모리: 74.4 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 01일 21:53:01
+2026년 08월 07일 17:59:29
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,42 +1,74 @@
+/*
+Q. ICN에서 출발하고, 주어진 항공권을 모두 사용하여 여행 경로를 만드세요.
+A. 가능 경로가 2개 이상이라면 알파벳 순서대로. 모든 도시는 꼭 방문 가능함.
+    - DFS를 통해서 완전 탐색/그래프 탐색
+    - 가능한 경로를 담을 수 있는 map 혹은 queue가 있으면 좋겠는데?
+
+*/
 import java.util.*;
 
 class Solution {
-    boolean[] visited;
-    List<String> routes = new ArrayList<>();
 
-    public String[] solution(String[][] tickets) {
+    static String[][] tickets;
+    static boolean[] visited;
+    static List<String> answer;
+
+    public String[] solution(String[][] input) {
+
+        tickets = input;
         visited = new boolean[tickets.length];
+        answer = new ArrayList<>();
 
-        // 알파벳 순으로 정렬
+        // 알파벳 순서가 빠른 경로부터 탐색하도록 정렬
         Arrays.sort(tickets, (a, b) -> {
-            if(a[0].equals(b[0])){
+            if (a[0].equals(b[0])) {
                 return a[1].compareTo(b[1]);
             }
+
             return a[0].compareTo(b[0]);
         });
 
-        dfs("ICN", "ICN", tickets, 0);
+        List<String> route = new ArrayList<>();
+        route.add("ICN");
 
-        return routes.get(0).split(" ");
+        dfs("ICN", 0, route);
+
+        return answer.toArray(new String[0]);
     }
 
-    private void dfs(String current, String path, String[][] tickets, int count){
-        if(count == tickets.length){ // 티켓 모두 사용하면 종료
-            routes.add(path);
-            return;
+    private boolean dfs(String start, int count, List<String> route) {
+
+        // 모든 티켓을 사용한 경우
+        if (count == tickets.length) {
+            answer = new ArrayList<>(route);
+            return true;
         }
 
-        for(int i=0; i<tickets.length; i++){
-            if(!visited[i] && tickets[i][0].equals(current)){
-                visited[i] = true;
-                dfs(
-                    tickets[i][1],
-                    path + " " + tickets[i][1],
-                    tickets,
-                    count + 1
-                );
-                visited[i] = false;
+        // 현재 도시에서 출발할 수 있는 티켓 찾기
+        for (int i = 0; i < tickets.length; i++) {
+
+            if (visited[i]) {
+                continue;
             }
+
+            if (!tickets[i][0].equals(start)) {
+                continue;
+            }
+
+            // 티켓 사용
+            visited[i] = true;
+
+            // 다음 도시 탐색
+            route.add(tickets[i][1]);
+            if (dfs(tickets[i][1], count + 1, route)) {
+                return true;
+            }
+
+            // 해당 경로가 실패했으면 원상복구
+            visited[i] = false;
+            route.remove(route.size() - 1);
         }
+
+        return false;
     }
 }
