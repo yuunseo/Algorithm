3c1ef1e6d81cafea45dde28862a0deb4eb5c9460
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-08-07 17:04:09 +0900

    [level 2] Title: 타겟 넘버, Time: 3.59 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nBUglikiHArJU44JCnKHqJ7F&gclid=Cj0KCQjw7cLOBhDmARIsAGsuA0nmzYce3DBjD2fgLiVMBTtfNAnPV5sv_9_l53PO7I_joABURuM2ZPkaAtHSEALw_wcB) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 87.7 MB, 시간: 4.86 ms
+메모리: 82.9 MB, 시간: 3.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 07:51:13
+2026년 08월 07일 17:04:05
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,34 @@
+/*
+Q. n개의 자연수들을 이용해서 타겟 넘버를 만드세요. 만들 수 있는 모든 경우의 수를 반환하기
+A. 모든 경우의 수를 구하는 알고리즘은 보통 DFS로 가지를 뻗어나가는 형태의 그래프 탐색을 이용.
+*/
+
 import java.util.*;
 
 class Solution {
-    static int result = 0; // 출력할 경우의 수
+    
+    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
          
-        dfs(numbers, 1, numbers[0], target); // (배열, 시작 인덱스, 총합)
-        dfs(numbers, 1, (-1)*numbers[0], target);
+        dfs(numbers, target, 0, numbers[0]); // 양수로 시작
+        dfs(numbers, target, 0, -numbers[0]); // 음수로 시작
         
-        return result;
+        return answer;
         
     }
     
-    static void dfs(int[] numbers, int idx, int total, int target){
+    private void dfs(int[] numbers, int target, int idx, int total){
         // 종료 조건
-        if(idx == numbers.length){ 
-            if(total == target) result ++;
+        if(idx+1 == numbers.length){
+            if(total == target) answer += 1;
             return;
         }
         
-        // 재귀 호출
-        dfs(numbers, idx+1, total + numbers[idx], target);
-        dfs(numbers, idx+1, total - numbers[idx], target);
+        // 재귀
+        dfs(numbers, target, idx+1, total + numbers[idx+1]);
+        dfs(numbers, target, idx+1, total - numbers[idx+1]);
+        
     }
+    
 }
