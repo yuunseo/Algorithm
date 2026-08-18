be95bff1535a038f85f2386ea19a05a9e648e94e
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-08-18 10:17:39 +0900

    [level 2] Title: 미로 탈출, Time: 4.14 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,95 +1,91 @@
+/*
+Q. 레버 칸을 지나, 미로를 나가는 데 걸리는 최단 시간 구하기 (BFS)
+A. S -> O -> L -> O -> E
+- O 통로 X 벽
+- 여러 번 지날 수 있으며, 탈출할 수 없으면 -1 반환
+- S -> L 까지 BFS로 최단 경로 구하고
+- L -> E 까지 BFS로 최단 경로 구해보자!
+*/
 import java.util.*;
 
 class Solution {
+    static int answer = 0;
+    static char[][] arr;
+    static int[] dy = {-1,1,0,0};
+    static int[] dx = {0,0,-1,1};
+    static int n;
+    static int m;
+    
     public int solution(String[] maps) {
-        int answer = 0;
-        int w = maps[0].length();
-        int h = maps.length;
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
         
-        // 2차원 배열로 치환하기
-        int[][] arr = new int[h][w];
-        int[] start = new int[2];
-        int[] end = new int[2];
-        int[] lever = new int[2];
-        for(int i=0; i<h; i++){
-            String str = maps[i];
-            for(int j=0; j<w; j++){
-                if(str.charAt(j) == 'O') arr[i][j] = 1;
-                else if(str.charAt(j) == 'X') arr[i][j] = 0;
-                else if(str.charAt(j) == 'S'){
-                    arr[i][j] = 2;
-                    start = new int[] {i,j};
-                }
-                else if(str.charAt(j) == 'E'){
-                    arr[i][j] = 3;
-                    end = new int[] {i,j};
-                }else if(str.charAt(j) == 'L'){
-                    arr[i][j] = 4;
-                    lever = new int[] {i,j};
+        // 좌표 저장
+        int startX=0; int startY=0;
+        int leverX=0; int leverY=0;
+        int endX=0; int endY=0;
+        
+        // 2차원 배열 초기화
+        n = maps.length;
+        m = maps[0].length();
+        arr = new char[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j] = maps[i].charAt(j);
+                
+                // 지점 좌표 구하기
+                if(arr[i][j] == 'S'){
+                    startY=i; startX=j;
+                }else if(arr[i][j] == 'L'){
+                    leverY=i; leverX=j;
+                }else if(arr[i][j] == 'E'){
+                    endY=i; endX=j;
                 }
             }
         }
         
-        // S -> L 최단거리 구하기
+        // S -> L BFS 구하기
+        int leverDistance = bfs(startY, startX, leverY, leverX);
+        if(leverDistance<0) return -1;
+        answer += leverDistance;
+        
+        // L -> E BFS 구하기
+        int endDistance = bfs(leverY, leverX, endY, endX);
+        if(endDistance<0) return -1;
+        answer += endDistance;
+        
+        return answer;
+        
+    }
+    
+    private int bfs(int sy, int sx, int ey, int ex){
+        boolean[][] visited = new boolean[n][m];
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[h][w];
-        q.add(new int[] {start[0],start[1],0});
-        visited[start[0]][start[1]] = true;
-        int start_to_lever = -1;
+        q.add(new int[] {sy, sx, 0});
+        visited[sy][sx] = true;
+        
         while(!q.isEmpty()){
             int[] cur = q.poll();
             
-            for(int d=0; d<4; d++){
-                int ny = cur[0] + dy[d];
-                int nx = cur[1] + dx[d];
-                
-                if(ny<0 || nx<0 || nx>=w || ny>=h) continue;
-                if(visited[ny][nx] || arr[ny][nx] == 0) continue;
-                
-                if(arr[ny][nx] == 4){ // 레버 도착 시 종료
-                    start_to_lever = cur[2]+1;
-                    break;
-                }else{
-                    q.add(new int[] {ny,nx,cur[2]+1}); // 다음 칸으로 이동
-                    visited[ny][nx] = true;
-                }
-                
+            // 종료
+            if(cur[0] == ey && cur[1] == ex){
+                return cur[2];
             }
-            if(start_to_lever != -1) break;
-        }
-        
-        // 레버에서 출구까지 최단거리
-        Queue<int[]> q2 = new LinkedList<>();
-        boolean[][] visited2 = new boolean[h][w];
-        q2.add(new int[] {lever[0],lever[1],0});
-        visited2[lever[0]][lever[1]] = true;
-        int lever_to_end = -1;
-        while(!q2.isEmpty()){
-            int[] cur = q2.poll();
             
+            // 상하좌우 확인하기
             for(int d=0; d<4; d++){
                 int ny = cur[0] + dy[d];
                 int nx = cur[1] + dx[d];
                 
-                if(ny<0 || nx<0 || nx>=w || ny>=h) continue;
-                if(visited2[ny][nx] || arr[ny][nx] == 0) continue;
+                if(ny<0 || nx<0 || 
+                   ny>=n || nx>=m ||
+                  arr[ny][nx] == 'X' ||
+                  visited[ny][nx]) continue;
                 
-                if(arr[ny][nx] == 3){ // 출구 도착 시 종료
-                    lever_to_end = cur[2]+1;
-                    break;
-                }else{
-                    q2.add(new int[] {ny,nx,cur[2]+1}); // 다음 칸으로 이동
-                    visited2[ny][nx] = true;
-                }
-                
+                q.add(new int[] {ny, nx, cur[2]+1});
+                visited[ny][nx] = true;
             }
-            if(lever_to_end != -1) break;
+            
         }
         
-        if(start_to_lever == -1 || lever_to_end == -1) return -1;
-        
-        return start_to_lever + lever_to_end;
+        return -1;
     }
 }
