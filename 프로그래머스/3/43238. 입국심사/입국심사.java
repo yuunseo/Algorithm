98197c64e64881ca9e0adcc945c17d8842b2e31d
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-08-21 11:23:28 +0900

    [level 3] Title: 입국심사, Time: 33.71 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,33 +1,48 @@
+/*
+Q. 모든 사람이 심사를 다 받는 최소 시간 구하기
+A. n명의 대기자, times.length명의 입국심사관
+    1) 1초씩 count하면서 times마다 n--, n==0되면 종료? 이렇게 하면 최대 100000까지 반복해야 함. 최악의 시간복잡도
+    2)  최소 시간은 1명 입국심사, 최대 시간은 n명이 입국심사, 이 안에서 이분탐색?
+*/
+import java.util.Arrays;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         
-        long min = Long.MAX_VALUE;
-        long max = Long.MIN_VALUE;
-        for(int time: times){
-            min = Math.min(min, time);
-            max = Math.max(max, time);
-        }
+        // times 정렬
+        Arrays.sort(times);
         
-        long left = min*1;
-        long right = max*n;
+        // 최소, 최대 시간 초기화
+        long min = 1;
+        long max = (long) times[times.length-1] * n;
         
-        while(left<=right){
-            long mid = (left+right)/2;
-            long sum = 0;
-            for(int time: times){
-                sum += (mid/time);
+        // 이분탐색
+        while(min <= max){
+            long mid = (min+max)/2;
+            
+            // mid 시간동안 몇 명을 심사할 수 있을까?
+            long people = 0;
+            for(long time: times){
+                people += mid / time;
             }
             
-            if(sum < n){
-                left = mid+1;
-            }else{
+            // n명 모두 할 수 있으면 성공
+            // 더 적은 시간으로 할 수 있는지 확인하기
+            if(people >= n){
                 answer = mid;
-                right = mid-1;
+                max = mid-1;
+            }
+
+            // n명 모두 심사하기에 부족하다면
+            // 더 많은 시간으로 확인하기
+            else{
+                min = mid+1;
             }
         }
         
-        
         return answer;
+        
+        
     }
 }
