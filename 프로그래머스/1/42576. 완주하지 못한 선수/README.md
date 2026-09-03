957e81244ca3d37a77f1093d1926db5c5dc6e7f0
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-03 21:05:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.29 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97.2 MB, 시간: 91.58 ms
+메모리: 122 MB, 시간: 57.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 19:39:24
+2026년 09월 03일 21:05:06
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,29 +1,27 @@
-/*
-- participant 참여자 completion 완주자
-- completion에 없는 참여자를 선택하면 됨. 동명이인이 있을 수 있음 주의 !!!!
-- 그럼 동명이인이더라도 한 명당 하나의 key를 갖도록 해주자
-
-*/
 import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> partic = new HashMap<>();
-
-        // 참가자 카운트
-        for (String name : participant) {
-            partic.put(name, partic.getOrDefault(name, 0) + 1);
+        // participants를 map 형태로 변경하기
+        Map<String, Integer> map = new HashMap<>();
+        for(String name: participant){
+            map.put(name, map.getOrDefault(name,0)+1);
         }
-
-        // 완주한 사람 차감
-        for (String name : completion) {
-            partic.put(name, partic.getOrDefault(name, 0) - 1);
+        
+        // completion에 존재하는 이름이면 map에서 제거하기
+        for(String name: completion){
+            if(map.get(name) > 0){
+                map.put(name, map.get(name)-1);
+            }
         }
-
-        // 값이 1(또는 >0)인 사람이 미완주자
-        for (Map.Entry<String, Integer> e : partic.entrySet()) {
-            if (e.getValue() > 0) return e.getKey();
+        
+        // 완주하지 못한 선수 찾기
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() >= 1) {
+                return entry.getKey();
+            }
         }
-        return ""; // 문제 조건상 도달하지 않음
+        
+        return "";
     }
 }
