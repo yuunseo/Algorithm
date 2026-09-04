bc6e10fcc2a2dc254a0daa9c9b28bb90570044af
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-04 14:44:01 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.06 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,30 +1,45 @@
 /*
-1) split으로 공백 구분 후, 각 단어에서 짝/홀 판단하기
-2) for문으로 s index로 가져와서, 공백일 때 인덱스 초기화하기
+[아이디어]
+1. split()을 통해 각 단어를 분리시킨다. - O(n)
+2. 각 단어를 모두 소문자로 바꾸고,index를 %2==0 짝수인 것만 대문자로 바꾼다. - O(n)
+3. 각 단어를 더해서 반환한다. - O(n)
+---> 공백이 여러 개 일 수도 있음
+1. s를 하나씩 꺼내서 처리한다. - O(n)
+2. 공백을 만나면 공백 그대로 추가하고, 초기화한다.
 */
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         
+        // 정답
         StringBuilder sb = new StringBuilder();
         
-        int initIndex = 0;
+       // 1. 한 글자씩 추출해서 소/대문자로 바꾸기
+        int idx = 0;
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == ' '){
-                initIndex = 0;
+            char cur = s.charAt(i);
+            
+            // 공백이라면, 공백만 추가
+            if(cur == ' '){
                 sb.append(' ');
+                idx = 0;
                 continue;
             }
             
-            if(initIndex % 2 == 0){
-                sb.append(Character.toUpperCase(s.charAt(i)));
+            // idx가 짝수라면, 대문자
+            // 아니라면, 소문자
+            if(idx % 2 == 0){
+                sb.append(Character.toUpperCase(cur));
+                idx+=1;
             }else{
-                sb.append(Character.toLowerCase(s.charAt(i)));
+                sb.append(Character.toLowerCase(cur));
+                idx+=1;
             }
-            
-            initIndex++;
-            
         }
         
+        // 정답 반환
         return sb.toString();
+        
     }
 }
