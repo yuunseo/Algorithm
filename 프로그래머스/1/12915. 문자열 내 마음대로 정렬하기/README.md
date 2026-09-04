6b1f4bf2127d959c6ded1652f5c0a66755d5f1c6
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-04 14:53:18 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.75 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 80.8 MB, 시간: 10.39 ms
+메모리: 72.1 MB, 시간: 1.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 23일 14:18:02
+2026년 09월 04일 14:53:14
 
 ### 문제 설명
 
--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,13 +1,38 @@
+/*
+[아이디어]
+m = 문자열 개수
+k = 문자열 최대 길이
 
+1. Arrays.sort()를 활용해 문자열을 정렬한다.
+   - 약 O(m log m)번 비교
+
+2. Comparator를 통해 정렬 기준을 지정한다.
+   - n번째 문자 비교 charAt(n): O(1)
+
+3. n번째 문자가 동일하다면 compareTo()로
+   문자열 전체를 사전순 비교한다.
+   - 최악의 경우 O(k)
+
+최종 시간복잡도
+= O(m log m) × O(k)
+= O(k × m log m)
+*/
 import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
         
-        Arrays.sort(strings, 
-                    Comparator.comparing((String s) -> s.charAt(n))
-                   .thenComparing(Comparator.naturalOrder()));
-
+        Arrays.sort(strings, (a, b) -> {
+            
+            // a와 b의 n번째 알파벳이 동일하다면, a와 b 자체 비교
+            if(a.charAt(n) == b.charAt(n)){
+                return a.compareTo(b);
+            }
+            
+            return a.charAt(n) - b.charAt(n); // 음수면 a가 앞, 양수면 b가 앞
+        });
+        
         return strings;
+     
     }
 }
