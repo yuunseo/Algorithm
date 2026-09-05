ba568129eb4daa02d81a47bffb9de0ab5f347f45
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-05 15:06:03 +0900

    [level 1] Title: 폰켓몬, Time: 2.50 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,31 +1,32 @@
 /*
-- 총 N마리 중 N/2마리 가져도 좋아 - 이때 종류의 개수가 많을 수록 좋음 ! 
-
-1. 총 마릿수 N, 가질 수 있는 마릿수 M = N/2
-2. nums를 돌면서 M개씩 조합을 구하기에는 시간 복잡도 매우 큼 !
-3. nums의 폰켓몬 종류를 key로하고 개수를 value로 해서 hashmap에 넣자
-4. 그 담에 map의 길이 >= M이라면, M 반환
-5. map 길이 < M이라면, map 길이 반환
-
+[아이디어]
+1. nums의 길이를 n으로 초기화하고, n/2을 limit으로 초기화한다. 
+2. nums에서 고를 수 있는 포켓몬의 종류의 개수 구하기. (Set)
+3. Set의 크기 vs limit. 
+4. set size가 더 크면, limit 반환. 
+5. set size가 더 작으면, set size 반환.
 */
-import java.util.Map;
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         
-        // 1. N과 M 초기화하기
-        int N = nums.length;
-        int M = N/2;
+        // 변수 초기화
+        int n = nums.length;
+        int limit = n/2;
         
-        // 2. nums를 hashmap으로 변환하기
-        Map<Integer, Integer> numsMap = new HashMap<>();
-        for(int i: nums){
-            numsMap.put(i, numsMap.getOrDefault(i,0)+1);
+        // set으로 개수 구하기
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            set.add(num);
         }
+        int size = set.size();
         
-        // 3. map의 길이와 M 비교
-        return (numsMap.size() >= M) ? M : numsMap.size();
+        // 비교하기
+        if(size >= limit){
+            return limit;
+        }else{
+            return size;
+        }
         
     }
 }
