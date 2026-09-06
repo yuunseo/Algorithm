252179a0dc7d731530cc3cebee35ea8f80865e3a
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-07 08:59:08 +0900

    [level 2] Title: 올바른 괄호, Time: 8.26 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 55.6 MB, 시간: 23.93 ms
+메모리: 58.3 MB, 시간: 8.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 09일 10:17:59
+2026년 09월 07일 08:59:04
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,28 @@
 /*
-올바른 괄호인지 판별하기-> stack
-( 여는 괄호는 push
-) 닫는 괄호는 peek해서 열린 괄호면 pop하기
-괄호가 남아있으면 false
+[아이디어]
+1. s에서 한 글자씩 읽어, Stask에 넣는다 -> O(n)
+2. 넣기 전, peek해서 짝이 맞으면 pop -> O(1)
+3. 모든 글자를 읽었는데 Stack이 남아있으면, 실패!
 */
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        
+        // stack 초기화
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        // 한 글자씩 읽기
         for(char c: s.toCharArray()){
-            if(c == '('){
+            if(stack.isEmpty() || c=='('){ // stack이 비었거나 여는 괄호면, 삽입
                 stack.push(c);
-            }else if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
+            } // 들어있던 문자와 한 쌍이면, 삭제
+            else if(c == ')' && stack.peek() == '('){ 
                 stack.pop();
-                continue;
-            }else{
-                return false;
             }
         }
-    
-        return !stack.isEmpty() ? false : true;
+        
+        return stack.isEmpty() ? true : false;
+       
     }
 }
