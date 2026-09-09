48c61d14ea479bd00f315def0f4e8b27499cbec7
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-09 10:08:03 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 0.03 ms
+메모리: 82.3 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 14일 06:17:13
+2026년 09월 09일 10:08:00
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,24 +1,18 @@
-/*
--> 3개를 더해서 0이 되는 경우의 수 모두 구하기
-1. 정렬 후 투포인터로 계산해보려고 했으나, 2개 비교가 아닌 3개 비교여서 포기
-2. 반복문 3개?
-*/
 import java.util.*;
 
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
         
-        for(int i=0; i<number.length-2; i++){
-            for(int j=i+1; j<number.length-1; j++){
+        int result = 0;
+      
+        for(int i=0; i<number.length; i++){
+            for(int j=i+1; j<number.length; j++){
                 for(int k=j+1; k<number.length; k++){
-                    int sum = number[i] + number[j] + number[k];
-                    
-                    if(sum == 0) answer++;
+                    if(number[i] + number[j] + number[k] == 0) result++;
                 }
             }
         }
         
-        return answer;
+        return result;
     }
 }
