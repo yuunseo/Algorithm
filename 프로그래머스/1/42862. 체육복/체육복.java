adc285c5f30a3249944c1296bc76d96f34c37a71
김윤서 <96402411+yuunseo@users.noreply.github.com>
2026-09-10 20:57:17 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,52 +1,50 @@
+/*
+[아이디어]
+1. 1~n까지 1로 초기화해서 배열 만들기 = initialization -O(N)
+2. lost 배열 돌면서 initialization에서 하나씩 제거하기 - O(R)
+3. reserve 배열 돌면서 본인, 앞, 뒤 사람 개수 확인하기 -O(1) -----> reserve 정렬을 위해 O(RlogR)
+4. 만약 안가져왔으면(0이면) 그 사람+1, 본인이 안가져왔으면 +1 - O(1)
+5. initialization을 다시 첨부터 돌면서 1이상인 사람 개수 count - O(N)
+*/
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 탐욕법; 현재 상황에서 최선의 선택을 한다
-        // try1. for문으로 인덱스를 주었는데, 두 배열이 길이가 달라서 index-error
-        // try2. List로 만들어서 빌려준 친구는 제외한다.
-        // + 두 배열 중 같은 학생은 제외한다.
-        // try3. 
         
-        // 수업을 들을 수 있는 학생 수
-        int canClass = n-lost.length;
+        int[] initialization = new int[n+1]; //공간복잡도 N+1
+        Arrays.fill(initialization, 1);
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        // 체육복이 없는 학생들의 리스트 생성
-        List<Integer> lostList = new ArrayList<>();
-        for (int std : lost) {
-            lostList.add(std);
-        }
-        // 여분 체육복을 갖는 학생들의 리스트 생성
-        List<Integer> reserveList = new ArrayList<>();
-        for (int std : reserve) {
-            reserveList.add(std);
+        for(int i: lost){
+            initialization[i] -= 1;
         }
         
-        // 두 리스트에서 중복하는 학생들 제외 & 수업 들을 수 있는 학생 수 증가
-        for (int std : lost) {
-            if (reserveList.contains(std)) {
-                lostList.remove(Integer.valueOf(std));
-                reserveList.remove(Integer.valueOf(std));
-                canClass++;
+        for(int i: reserve){
+            initialization[i] += 1;
+        }
+        
+        
+        for(int i=1; i<=n; i++){
+            if(initialization[i] <2) continue;
+            
+            // 앞 사람 확인
+            if(i-1 > 0 && initialization[i-1] == 0){
+                initialization[i-1] += 1;
+                initialization[i] -= 1;
+            }
+            
+            // 뒷 사람 확인
+            else if(i+1 <= n && initialization[i+1] == 0){
+                initialization[i+1] += 1;
+                initialization[i] -= 1;
             }
         }
         
-        
-         // 여분의 체육복을 빌려주는 과정
-        for (int i = 0; i < reserveList.size(); i++) {
-            int std = reserveList.get(i);
-            if (lostList.contains(std - 1)) {
-                canClass++;
-                lostList.remove(Integer.valueOf(std - 1));
-            } else if (lostList.contains(std + 1)) {
-                canClass++;
-                lostList.remove(Integer.valueOf(std + 1));
-            }
+        int result = 0;
+        for(int i=1; i<=n; i++){
+            if(initialization[i] >= 1) result+=1;
         }
         
-        return canClass;
+        return result;
+     
     }
 }
